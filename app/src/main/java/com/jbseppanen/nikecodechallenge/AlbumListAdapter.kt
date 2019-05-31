6dde91b9b2b01b49e487bf1207aaa1c4b2a68605
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-31 16:14:40 -0700

    Load image into recycler view elements using Glide.

--- app/src/main/java/com/jbseppanen/nikecodechallenge/AlbumListAdapter.kt
+++ app/src/main/java/com/jbseppanen/nikecodechallenge/AlbumListAdapter.kt
@@ -1,14 +1,21 @@
 package com.jbseppanen.nikecodechallenge
 
 import android.app.Activity
+import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 
-class AlbumListAdapter(val activity: Activity): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class AlbumListAdapter(val activity: Activity) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     class AlbumItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val albumImage: ImageView = view.findViewById(R.id.image_album_art)
@@ -24,18 +31,23 @@ class AlbumListAdapter(val activity: Activity): RecyclerView.Adapter<RecyclerVie
     }
 
     private fun getItems() {
-            DataDao.getAlbums(object : DataDao.DataCallback {
-                override fun callback(albums: ArrayList<Album>) {
-                    data.addAll(albums)
-                    activity.runOnUiThread { notifyDataSetChanged() }
-                }
-            })
+        DataDao.getAlbums(object : DataDao.DataCallback {
+            override fun callback(albums: ArrayList<Album>) {
+                data.addAll(albums)
+                activity.runOnUiThread { notifyDataSetChanged() }
+            }
+        })
     }
 
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return AlbumItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.album_item_layout, parent, false))
+        return AlbumItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.album_item_layout,
+                parent,
+                false
+            )
+        )
     }
 
     override fun getItemCount(): Int {
@@ -48,5 +60,31 @@ class AlbumListAdapter(val activity: Activity): RecyclerView.Adapter<RecyclerVie
 
         albumHolder.albumNameView.text = element.name
         albumHolder.artistNameView.text = element.artistName
+
+        Glide
+            .with(activity)
+            .load(element.artworkUrl100)
+            .apply(RequestOptions().centerCrop())
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+            })
+            .into(albumHolder.albumImage)
     }
 }
