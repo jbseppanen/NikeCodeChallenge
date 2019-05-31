2105d963ea8b36defd13fb679b594984e65d43b9
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-31 14:53:04 -0700

    Implement data models.

---
+++ app/src/main/java/com/jbseppanen/nikecodechallenge/Models.kt
@@ -0,0 +1,22 @@
+package com.jbseppanen.nikecodechallenge
+
+data class Album(
+    val artistId: String,
+    val artistName: String,
+    val artistUrl: String,
+    val artworkUrl100: String,
+    val contentAdvisoryRating: String,
+    val copyright: String,
+    val genres: List<Genre>,
+    val id: String,
+    val kind: String,
+    val name: String,
+    val releaseDate: String,
+    val url: String
+)
+
+data class Genre(
+    val genreId: String,
+    val name: String,
+    val url: String
+)
