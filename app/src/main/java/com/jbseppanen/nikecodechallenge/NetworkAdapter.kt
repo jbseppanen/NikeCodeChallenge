59b59ce9f302c2b5f837abc2be1e53c556b829ea
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-31 13:21:00 -0700

    Implement Network Adaptor.

---
+++ app/src/main/java/com/jbseppanen/nikecodechallenge/NetworkAdapter.kt
@@ -0,0 +1,80 @@
+package com.jbseppanen.nikecodechallenge
+
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+
+object NetworkAdapter {
+    const val GET = "GET"
+    const val POST = "POST"
+    const val PUT = "PUT"
+    const val DELETE = "DELETE"
+    const val TIMEOUT = 3000
+
+
+    interface NetworkCallback {
+        fun returnResult(success: Boolean?, result: String)
+    }
+
+    fun httpRequest(stringUrl: String, requestType: String, jsonBody: String?, callback: NetworkCallback) {
+        Thread(Runnable {
+            var result = ""
+            var success = false
+            var stream: InputStream? = null
+            var connection: HttpURLConnection? = null
+            try {
+                val url = URL(stringUrl)
+                connection = url.openConnection() as HttpURLConnection
+                connection.readTimeout = TIMEOUT
+                connection.connectTimeout = TIMEOUT
+                connection.requestMethod = requestType
+                connection.setRequestProperty("Content-Type", "application/json")
+
+                if (requestType == GET || requestType == DELETE) {
+                    connection.connect()
+                } else if (requestType == POST || requestType == PUT) {
+                    if (jsonBody != null) {
+                        val outputStream = connection.outputStream
+                        outputStream.write(jsonBody.toByteArray())
+                        outputStream.close()
+                    }
+                }
+
+                if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                    stream = connection.inputStream
+                    if (stream != null) {
+                        val reader = BufferedReader(InputStreamReader(stream))
+                        val builder = StringBuilder()
+                        var line: String? = reader.readLine()
+                        while (line != null) {
+                            builder.append(line)
+                            line = reader.readLine()
+                        }
+                        result = builder.toString()
+                        success = true
+                    }
+                }
+
+            } catch (e: MalformedURLException) {
+                e.printStackTrace()
+                result = e.message.toString()
+            } catch (e: IOException) {
+                e.printStackTrace()
+                result = e.message.toString()
+            } finally {
+                connection?.disconnect()
+
+                if (stream != null) {
+                    try {
+                        stream.close()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                }
+            }
+            callback.returnResult(success, result)
+        }).start()
+    }
+}
